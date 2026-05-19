a89332813943441d54177e97d1f60c34186c5055
JossyE <joeyeffiong@gmail.com>
2026-05-19 12:09:30 +0100

    quick fix

--- src/components/Product.svelte
+++ src/components/Product.svelte
@@ -39,7 +39,7 @@
 
 </script>
 <SectionWrapper id='products'>
-    <div class="flex flex-col gap-10 sm:gap-14 md:gap-24 flex-1 items-center justify-center pb-10 md:pb-14">
+    <div class="flex flex-col gap-10 sm:gap-14 md:gap-24 flex-1 items-center justify-center pb-10 md:pb-14 mt-10">
         <div class="flex flex-col gap-2">
             <p class="opacity-60 text-base sm:text-lg text-orange-400 md:text-xl text-center">
                 Smile Services
